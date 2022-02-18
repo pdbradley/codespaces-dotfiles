663612ac9b0e43abc47d9985a2e9f417e7dadede
Philip Bradley <pdbradley@gmail.com>
2022-02-18 07:21:34 -0500

    add zshrc custom folder

---
+++ .config/oh-my-zsh/custom/functions.zsh
@@ -0,0 +1,25 @@
+function g {
+   if [[ $# > 0 ]]; then
+     git $@
+   else
+     git status
+   fi
+}
+
+# to alter priority of tasks in task warrior
+function taskh {
+  t modify $1 priority:H
+}
+function taskm {
+  t modify $1 priority:M
+}
+function taskl {
+  t modify $1 priority:L
+}
+function taskp {
+  t modify $1 priority:$2
+}
+
+function ezf {
+  find . -name $1 -print
+}
---
+++ .config/oh-my-zsh/custom/paths.zsh
@@ -0,0 +1,21 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+export VISUAL=nvim
+# export PATH="/Users/philip/dotfiles/bin:$PATH"
+
+# export PATH="$HOME/bin:$PATH"
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+export PATH="/usr/local/mysql/bin:$PATH"
+
+# postgres
+# export PATH="/usr/local/opt/postgresql@13/bin:$PATH"
+
+export PKG_CONFIG_PATH="/usr/local/opt/pkg-config:$PKG_CONFIG_PATH"
+export EDITOR='nvim'
