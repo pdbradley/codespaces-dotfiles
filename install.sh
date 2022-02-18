cea148394fcab1ce1ee7f9157daa056155dcab9a
Philip Bradley <pdbradley@gmail.com>
2022-02-18 07:04:26 -0500

    add tmux conf

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # this script will be run by codespaces
 # after importing this dotfile repo into a codespace
 
-
 # tmux and neovim
 apt install -y tmux
 apt install -y neovim
@@ -17,10 +16,29 @@ apt install -y ripgrep
 apt install -y fzf
 apt install -y silversearcher-ag
 
-# basically copying the dotfiles within this repo
-# putting them in the right place in the codespace
-cp -r .config/* ~/.config
+#-------------------------------------------------------------
+# basically copying the dotfiles within the root of this repo
+# putting them in the home dir in the codespace
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
 
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+create_symlinks
+#-------------------------------------------------------------
+# recursive copy of .config/ in this repo into the home folder
+cp -r .config/* ~/.config
+#-------------------------------------------------------------
 
 # install all the plugins in neovim and quit
 nvim +PlugInstall +qall
