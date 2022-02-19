31eeac2f2da5a14ffa7739672c6a751ba33469c4
Philip Bradley <pdbradley@gmail.com>
2022-02-19 08:13:57 -0500

    nargh

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@
 # after importing this dotfile repo into a codespace
 
 # tmux and neovim
-apt install -y tmux
-apt install -y neovim
+apt-get install -y tmux
+apt-get install -y neovim
 
 # install neovim plugin manager.  dotfiles will be imported via personal codespace settings
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
 # install ripgrep and fzf and ag (nvim uses them)
-apt install -y ripgrep
-apt install -y fzf
-apt install -y silversearcher-ag
+apt-get install -y ripgrep
+apt-get install -y fzf
+apt-get install -y silversearcher-ag
 
 #-------------------------------------------------------------
 # basically copying the dotfiles within the root of this repo
