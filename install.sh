0fe6295be3e1ace41f60d3ef6dc2e9a53e4ae6ed
Philip Bradley <pdbradley@gmail.com>
2022-02-22 12:12:26 -0500

    nargh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ rm -rf "$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #copy my custom oh my zsh stuff
-cp -r ./config/oh-my-zsh/* ~/.oh-my-zsh
+cp -r .config/oh-my-zsh/* ~/.oh-my-zsh
 
 # don't know why this line below does not work
 # sudo chsh -s /usr/bin/zsh $USERNAME
