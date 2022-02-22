7f43c9cde1fb9e958c0d46b0e40fc9a53f0f030a
Philip Bradley <pdbradley@gmail.com>
2022-02-22 13:42:24 -0500

    install sudo

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ cp -r .config/* ~/.config
 # install all the plugins in neovim and quit
 nvim +PlugInstall +qall
 
-# install zsh
-sudo apt-get install -y zsh
+# install zsh and sudo
+sudo apt-get install -y zsh sudo
 # install oh my zsh
 rm -rf "$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
