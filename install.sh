13668ec5c052d55bc04d3768116b3a2dd94de9fc
Philip Bradley <pdbradley@gmail.com>
2022-02-22 11:50:52 -0500

    hardcode codespace

--- install.sh
+++ install.sh
@@ -50,8 +50,8 @@ rm -rf "$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-#sudo chsh -s /usr/bin/zsh codespace
-sudo chsh -s /usr/bin/zsh $USERNAME
+sudo chsh -s /usr/bin/zsh codespace
+# sudo chsh -s /usr/bin/zsh $USERNAME
 
 
 
