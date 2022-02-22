6b0fc542d502936c9c7b183a4107568bfb1c8476
Philip Bradley <pdbradley@gmail.com>
2022-02-22 11:15:02 -0500

    username var

--- install.sh
+++ install.sh
@@ -49,7 +49,9 @@ sudo apt-get install -y zsh
 rm -rf "$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo chsh -s /usr/bin/zsh codespace
+
+#sudo chsh -s /usr/bin/zsh codespace
+sudo chsh -s /usr/bin/zsh $USERNAME
 
 
 
